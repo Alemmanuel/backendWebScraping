346dce7c224b2ab307231a7479eb80f6a5075aca
Emmanuel <alemmanuel0412@gmail.com>
2025-03-17 11:29:34 -0500

    Primer Commit

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const puppeteer = require("puppeteer-core");
 const chromium = require("@sparticuz/chromium"); // Nueva dependencia
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.get("/api/search", async (req, res) => {
